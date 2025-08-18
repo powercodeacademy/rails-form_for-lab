4e94376a5ca07b7bc05e3637e91164e1fd164dcd
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-18 13:10:08 -0400

    Build create and update actions for students and school_classes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,4 +6,16 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    @student = Student.create(params[:first_name], params[:last_name])
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params[:first_name], params[:last_name])
+    redirect_to student_path(@student)
+  end
 end
