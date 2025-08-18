11f8cf4a3cc79673654145a1fdbae93ae51efe75
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 14:22:00 -0400

    Edit a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,9 +13,13 @@ class StudentsController < ApplicationController
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
   end
 
   private 
