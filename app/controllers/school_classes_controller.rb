cee6a612b7617c736b10ba7ad307ef3ac466bba6
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 14:15:18 -0400

    Update school class and redirect to updated page

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,9 +13,13 @@ class SchoolClassesController < ApplicationController
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
   end
 
   private 
