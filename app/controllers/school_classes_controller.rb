e7af71de250b990921a0c9564d0e2764c7721f81
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-18 13:48:48 -0400

    Build new school_class form, submit content and render to page

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,15 +7,25 @@ class SchoolClassesController < ApplicationController
     @school_class = SchoolClass.find(params[:id])
   end
 
+  def new
+    @school_class = SchoolClass.new
+  end
+
   def create
-    @school_class = SchoolClass.new(params[:title], params[:room_number])
+    @school_class = SchoolClass.new(school_class_params)
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(params[:title], params[:room_number])
+    @school_class.update(school_class_params)
     redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
   end
 end
