ba97b6aea3b3c26319c72c154a66a8b9fd5c0d42
Nihar Patel <niharpatel52@gmail.com>
2025-08-18 10:31:34 -0400

    Create Students and StudentClasses controllers and define routes

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,31 @@
 class SchoolClassesController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = Post.new
+    @school_class.title = params[:title]
+    @school_class.description = params[:description]
+    @school_class.save
+    redirect_to post_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class).permit(:title, :room_number))
+    redirect_to post_path(@school_class)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-
 Rails.application.routes.draw do
-  resources :school_classes
-  resources :students, only: [:index, :show, :new, :create, :edit, :update]
-  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
+  resources :students, only: %i[index show new create edit update]
+  resources :school_classes, only: %i[index show new create edit update]
 end
