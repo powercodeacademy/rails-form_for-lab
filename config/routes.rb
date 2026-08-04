673ef5d5ad62cedbcb62bc3b2289dab6f27eaa90
LiamKirkland <liam.b.kirkland@gmail.com>
2026-08-04 10:50:33 -0400

    form_for lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   resources :students, only: [:index, :show, :new, :create, :edit, :update]
   resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
